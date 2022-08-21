98d5d43c6e45ed04b6c1d79247a160d63e0f331b
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-08-22 03:33:53 +0800

    Add black background class to grid items on mouse hover

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const gridSize = 16;
 const gridContainer = document.querySelector("#container");
 
 for (let i = 0; i < gridSize * gridSize; i++) {
-  const box = document.createElement("div");
-  box.classList.add("grid-box");
-  gridContainer.appendChild(box);
+   const box = document.createElement("div");
+   box.classList.add("grid-box");
+   gridContainer.appendChild(box);
+
+   box.addEventListener("mouseover", () => {
+      box.classList.add("black-bg");
+   });
 }
