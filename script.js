0625071ed5e846e3e20c06b248933264e0d89092
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-08-22 03:49:04 +0800

    Put the task of drawing grid in a function

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const gridSize = 16;
 
-const gridContainer = document.querySelector("#container");
+function drawGrid() {
+   const gridContainer = document.querySelector("#container");
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-   const box = document.createElement("div");
-   box.classList.add("grid-box");
-   gridContainer.appendChild(box);
+   for (let i = 0; i < gridSize * gridSize; i++) {
+      const box = document.createElement("div");
+      box.classList.add("grid-box");
+      gridContainer.appendChild(box);
 
-   box.addEventListener("mouseover", () => {
-      box.classList.add("black-bg");
-   });
+      box.addEventListener("mouseover", () => {
+         box.classList.add("black-bg");
+      });
+   }
 }
